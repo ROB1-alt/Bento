3a517c05f77983c2b665a39c9411b8e05f941b13
Rob1 <robin.pautigny@hetic.net>
2021-05-21 13:52:36 +0200

    huzb

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = 'Rob1';
+const name = ' John Doe';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
